fb4d208e89bd10ad9a50669831075a4f133369f7
Nicole Pessoa <nicole_pereira-rj@hotmail.com>
2021-05-17 17:41:32 -0300

    Projeto inicial aula 01

---
+++ src/ProgramaName.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProgramaName {
+
+	public static void main (String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		String nome;
+		
+		System.out.println("Por favor,digite seu nome:");
+		nome = leitor.next();
+		
+	}
+}
